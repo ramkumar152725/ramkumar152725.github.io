79977ff537ba4937f75182f44f873b4beb9f272a
ramkumar152725 <ramkumar152725@gmail.com>
2021-03-16 10:57:26 +0530

    update

---
+++ module 4 solution/mod-4-sol/speakhello.js
@@ -0,0 +1,18 @@
+
+(function(){
+ var helloSpeaker = {};
+ 
+
+
+var speakWord = "Hello";
+helloSpeaker.speak = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+
+
+
+window.helloSpeaker = helloSpeaker;
+
+})();
+
+
